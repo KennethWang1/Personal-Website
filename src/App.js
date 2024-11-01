c352296d22287c7510c8047311d242555cb1c379
KennethWang1 <122188744+KennethWang1@users.noreply.github.com>
2024-11-01 10:17:48 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <body>
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Tiny5&display=swap');
+    </style>
+      <p>&gt; Hello! Welcome to Kenneth's webiste. Type --help if you want to see a list of commands.</p>
+
+      <label for = "text">&gt; </label>
+      <input type="text" name = "text"></input>
+    </body>
+  );
+}
+
+export default App;
